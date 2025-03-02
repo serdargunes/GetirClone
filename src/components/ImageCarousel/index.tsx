da498de5d53209d58c0ef4ba3abd214aec1887b5
Serdargunes <95936184+serdargunes@users.noreply.github.com>
2025-03-02 21:16:45 +0300

    images dot update

--- src/components/ImageCarousel/index.tsx
+++ src/components/ImageCarousel/index.tsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import { FlatList,View,Image,Dimensions } from 'react-native'
+import React,{useState} from 'react'
+import { FlatList,View,Image,Dimensions,StyleSheet } from 'react-native'
 
 const {width,height} = Dimensions.get("window")
 
+
 function index({images}:{images:string[]}) {
+    const [activeIndex, setActiveIndex] = useState(0)
+    const onViewRef = React.useRef((viewableItems) =>{
+        if(viewableItems.viewableItems.length > 0)
+        {
+            setActiveIndex(viewableItems.viewableItems[0].index || 0)
+        }
+    })
+    const viewConfigRef = React.useRef({viewAreaCoveragePercentThreshold:50,})
   return (
-    <View>
+    <View style = {{alignItems:'center',width:'100%',backgroundColor:'white',height:height*0.25,paddingTop:25}}>
    <FlatList
    data={images}
-   style ={{width:width*0.5,height:height*0.2}}
+   style ={{width:width*0.5,height:height*0.21}}
    renderItem={(item) =>(
     <Image 
     source={{uri:item.item}}
     
-    style ={{width:width*0.5,height:height*0.20}}
+    style ={{width:width*0.5,height:height*0.21,resizeMode:'stretch'}}
     />
    )}
    horizontal
@@ -21,12 +30,28 @@ function index({images}:{images:string[]}) {
    snapToInterval={width}
    snapToAlignment={"center"}
    decelerationRate={'fast'}
+   viewabilityConfig={viewConfigRef.current}
+   onViewableItemsChanged={onViewRef.current}
    >
 
 
    </FlatList>
+   <View>
+    <View>
+        {images.map((image,index) => (
+            <View key = {index} style = {styles.dot} />
+        ))}
+    </View>
+   </View>
    </View>
   )
 }
+const styles = StyleSheet.create({
+dot:{
+    width:8,
+    height:8,
+    borderRadius:20,
+},
+})
 
 export default index;
